e43a3ea3e85a02433b12b7aa9c41e9fb5ef30118
Barry McDermid <bmcd77@hotmail.com>
2020-10-16 16:21:12 +0100

    I'm adding a new bat file to Github

---
+++ aNotherNewFile.bat
@@ -0,0 +1,9 @@
+echo on
+echo "this is a new file. Fies in this dir : "
+echo 
+
+#Run dir command to list files
+Dir -l
+
+#wait for the person to close the cmd window manually so they can see the files listed
+pause
